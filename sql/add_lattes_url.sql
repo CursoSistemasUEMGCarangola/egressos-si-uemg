a565ba1b2da8c106207dc8c9e77e1475104a3148
Nilton <niltonfjunior@gmail.com>
2026-02-18 10:09:22 -0300

    Limpeza de scripts sql antigos

--- sql/add_lattes_url.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.profiles
-ADD COLUMN IF NOT EXISTS lattes_url text;
-
-COMMENT ON COLUMN public.profiles.lattes_url IS 'URL do Currículo Lattes do egresso';
--- sql/add_mobile_phone_column.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.profiles
-ADD COLUMN IF NOT EXISTS mobile_phone text;
-
-COMMENT ON COLUMN public.profiles.mobile_phone IS 'Número de telefone celular do egresso';
